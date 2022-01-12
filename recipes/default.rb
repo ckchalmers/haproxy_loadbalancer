9ff8fb582a13c37764fbe30d6678fecdb24ace86
ckchalmers <cchalmers@pagerduty.com>
2022-01-12 19:07:12 +0000

    Step 26 again

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,8 +10,7 @@ haproxy_frontend 'http-in' do
   bind '*:80'
   default_backend 'servers'
 end
-web_node_objects = search('node', "policy_name:webserver AND
-policy_group:#{node['policy_group']}")
+web_node_objects = search('node', "policy_name:webserver AND policy_group:#{node['policy_group']}")
 web_servers = []
 web_node_objects.each do |one_node|
   server = "#{one_node['cloud']['public_hostname']} #{one_node['cloud']['public_ipv4']}:80 check"
